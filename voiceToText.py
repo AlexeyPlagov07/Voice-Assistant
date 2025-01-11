da71d7ee85025b23d28605d24504aca4fa445f90
AlexeyPlagov07 <alexeyplagov@gmail.com>
2025-01-10 19:50:48 -0500

    added emailing aspect as well as contact book for emails and has a phone number variable

--- voiceToText.py
+++ voiceToText.py
@@ -1,7 +1,14 @@
 import pyttsx3
 import datetime
 import speech_recognition as sr
+import smtplib
 
+class person:
+    def __init__(self, name, phone, email):
+        self.name = name
+        self.phone = phone
+        self.email = email
+person_dict = {}
 MASTER = 'Alexey'
 print('Initializing...')
 engine = pyttsx3.init('sapi5')
@@ -30,7 +37,67 @@ def date():
 def no():
     global stop_second_loop
     stop_second_loop = 1
-command_list = {'time':time, 'date':date, "no":no}
+
+def add_to_dict():
+    name, phone, email = input("Enter name, phone number, and email: ").split()
+    x = name
+    name = person(name, phone, email)
+    person_dict[x] = name
+    print(person_dict)
+def wishme():
+   speak("Welcome back sir!")
+   speak("Charles at your service. Please tell me how can i help you?")
+
+def sendEmail():
+    print(person_dict)
+    recognizer = sr.Recognizer()
+    text1 = None
+    # Use the microphone as the source for input
+    while text1 == None:
+        with sr.Microphone() as source:
+            speak("Who do you want to email?")
+            audio = recognizer.listen(source)
+
+            try:
+            # Recognize speech using Google Web Speech API
+                text1 = recognizer.recognize_google(audio)
+                print(text1)
+            except sr.UnknownValueError:
+                pass
+            except sr.RequestError:
+                pass
+    if text1 in person_dict:
+        content = None
+        while content == None:
+            with sr.Microphone() as source:
+                speak("What do you want to email?")
+            
+                audio = recognizer.listen(source)
+        
+                try:
+                # Recognize speech using Google Web Speech API
+                    content = recognizer.recognize_google(audio)
+                    print(content)
+                    speak("Emailing")
+                    speak(content)
+                    speak("to")
+                    speak(text1)
+                except sr.UnknownValueError:
+                    pass
+                except sr.RequestError:
+                    pass
+    
+        server = smtplib.SMTP('smtp.gmail.com', 587)
+        server.ehlo()
+        server.starttls()
+        server.login('alexeyplagov@gmail.com', 'snjq yyae bjlf jvgn')
+        server.sendmail('alexeyplagov@gmail.com', person_dict[text1].email, content)
+        server.close()
+    else:
+        speak("Type the details in the console.")
+        add_to_dict()
+        sendEmail()
+command_list = {'time':time, 'date':date, "no":no, "email":sendEmail}
 def repeat():
 
     # Initialize recognizer
@@ -70,7 +137,7 @@ while True:
         text = recognizer.recognize_google(audio)
         print(text)
         if 'charles' in text.lower():
-            speak("How can I help?")
+            wishme()
             stop_second_loop = 0
             while stop_second_loop == 0:
                 repeat()
